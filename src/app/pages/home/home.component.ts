6609383ed6e3711457601481e46f7641ec2eb953
Andres Hortua <fandresh7@gmail.com>
2024-08-10 22:45:22 -0500

    add filter form and defer to countries list

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,15 +1,17 @@
 import { Component, inject } from '@angular/core'
 import { CountriesService } from '../../services/countries.service'
 import { CountriesListComponent } from '../../components/countries-list/countries-list.component'
+import { FilterFormComponent } from '../../components/filter-form/filter-form.component'
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CountriesListComponent],
+  imports: [CountriesListComponent, FilterFormComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
   private countriesService = inject(CountriesService)
   countries = this.countriesService.countries
+  loading = this.countriesService.loading
 }
--- src/app/services/countries.service.ts
+++ src/app/services/countries.service.ts
@@ -12,23 +12,42 @@ export class CountriesService {
   #countriesSignal = signal<Country[]>([])
   countries = this.#countriesSignal.asReadonly()
 
+  #loadingSignal = signal(false)
+  loading = this.#loadingSignal.asReadonly()
+
   constructor() {
-    this.initCountries()
+    this.getCountries()
   }
 
-  private async initCountries() {
+  async getCountries(search?: string, region?: string) {
+    search = search?.toLowerCase().trim()
+
     const countries = await this.fetchCountries()
-    this.#countriesSignal.set(countries)
+
+    const filteredCountries = countries.filter(country => {
+      const searchCondition = !search || country.name.toLowerCase().includes(search)
+      const regionCondition = !region || country.region === region
+
+      return searchCondition && regionCondition
+    })
+
+    this.#countriesSignal.set(filteredCountries)
   }
 
   private async fetchCountries() {
+    this.#loadingSignal.set(true)
+
     const countries$ = this.http.get<Country[]>('./data.json')
-    return await firstValueFrom(countries$)
+    const countries = await firstValueFrom(countries$)
+
+    this.#loadingSignal.set(false)
+
+    return countries
   }
 
   async getCountry(code: string) {
     if (this.countries().length === 0) {
-      await this.initCountries()
+      await this.getCountries()
     }
 
     return this.countries().find(country => country.alpha2Code === code) ?? null
@@ -36,7 +55,7 @@ export class CountriesService {
 
   async getCountryBorders(country: Country) {
     if (this.countries().length === 0) {
-      await this.initCountries()
+      await this.getCountries()
     }
     return this.countries().filter(c => country.borders?.includes(c.alpha3Code))
   }
